583694011bc37bea2a8f8cef7d69baab04f18765
Alan Daniel <20223tn018@utez.edu.mx>
2025-07-10 16:18:34 -0600

    Project upload

--- App.js
+++ App.js
@@ -3,9 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, flexDirection: 'column-reverse' }}>
+      <View style={{ flex: 0.2, backgroundColor: 'blue', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+        <Text style={styles.redText}>text1</Text>
+        <Text style={styles.redText}>text2</Text>
+      </View>
+      <View style={{ flex: 1, backgroundColor: 'white' }}></View>
+      <View style={{ flex: 0.2, backgroundColor: 'red', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+        {/* justify content es eje Y */}
+        {/* align items es eje X */}
+        <Text style={styles.redText}>text1</Text>
+        <Text style={styles.redText}>text2</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +25,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  redText: {
+    flex: 1,
+    color: '#fff',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 20,
   },
 });
